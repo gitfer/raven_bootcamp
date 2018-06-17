13b9fdfca67535fc2422cca56f4bbf487fd4a9c2
Federico Rota <federico.rota01@gmail.com>
2018-06-17 18:25:53 +0200

    First commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace raven_bootcamp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
