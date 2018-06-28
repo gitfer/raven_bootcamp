a203138dd83f6ccda13e9e38f4204e279ff6c097
Federico Rota <federico.rota01@gmail.com>
2018-06-28 17:04:50 +0200

    Started using ravendb

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Raven.Client.Documents;
 
 namespace raven_bootcamp
 {
@@ -7,6 +8,20 @@ namespace raven_bootcamp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            var documentStore = new DocumentStore
+            {
+                Urls = new[] { "http://localhost:8080" },
+                Database = "Northwind"
+            };
+
+            documentStore.Initialize();
+
+            using (var session = documentStore.OpenSession())
+            {
+                var p = session.Load<dynamic>("products/1-A");
+                System.Console.WriteLine(p.Name);
+            }
         }
     }
 }
