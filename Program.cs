d9809c9c696659516161b47df88207cf6b1a887d
Federico Rota <federico.rota01@gmail.com>
2018-07-09 23:12:59 +0200

    U1:Lesson 6 Query and RQL - U1:Lesson 7 CRUD

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Diagnostics;
 using System.Linq;
 using Raven.Client.Documents;
 using NorthwindModels;
+using System;
 
 namespace raven_bootcamp
 {
@@ -10,23 +11,70 @@ namespace raven_bootcamp
     {
         static void Main(string[] args)
         {
+            // LOAD documents and related documents
+            WriteLine("Please, enter an order # (e.g. 7. 0 to exit): ");
 
-            while (true)
+            int orderNumber;
+            if (!int.TryParse(ReadLine(), out orderNumber))
             {
-                WriteLine("Please, enter an order # (0 to exit): ");
-
-                int orderNumber;
-                if (!int.TryParse(ReadLine(), out orderNumber))
-                {
-                    WriteLine("Order # is invalid.");
-                    continue;
-                }
-
-                if (orderNumber == 0) break;
-
-                PrintOrder(orderNumber);
+                WriteLine("Order # is invalid.");
             }
 
+            if (orderNumber == 0)
+            {
+                throw new ArgumentException($"OrderNumber {orderNumber} not valid");
+            }
+
+            PrintOrder(orderNumber);
+
+            // QUERY and RQL
+            WriteLine("Now, enter a companyId # (e.g. 39. 0 to exit) to find orders by company id: ");
+
+            int companyId;
+            if (!int.TryParse(ReadLine(), out companyId))
+            {
+                WriteLine("Company # is invalid.");
+            }
+
+            if (companyId == 0)
+            {
+                throw new ArgumentException($"Company Id {companyId} not valid");
+            }
+
+            QueryOrdersForCompanyId(companyId);
+
+            // C(r)UD
+            // SaveChanges should be called only once per session.
+            WriteLine("C(r)UD example");
+            Category category;
+            string id;
+            using (var session = DocumentStoreHolder.Store.OpenSession())
+            {
+                // Create
+                category = new Category
+                {
+                    Description = "Descrizione Categoria Fede",
+                    Name = "Categoria Fede"
+                };
+                session.Store(category);
+                session.SaveChanges();
+            }
+
+            using (var session = DocumentStoreHolder.Store.OpenSession())
+            {
+                // Update
+                id = category.Id;
+                var savedCategory = session.Load<Category>(id);
+                savedCategory.Name = savedCategory.Name + " UPDATED";
+                session.SaveChanges();
+            }
+
+            using (var session = DocumentStoreHolder.Store.OpenSession())
+            {
+                // Delete
+                session.Delete(id);
+                session.SaveChanges();
+            }
             WriteLine("Goodbye!");
         }
 
@@ -61,6 +109,42 @@ namespace raven_bootcamp
                               $" {orderLine.Quantity} x {p.QuantityPerUnit}");
                 }
             }
+        }
+
+        private static void QueryOrdersForCompanyId(int companyId)
+        {
+            using (var session = DocumentStoreHolder.Store.OpenSession())
+            {
+
+                // var orders = (from order in session.Query<Order>()
+                //             .Include(o => o.Company)
+                //               where order.Company == $"companies/{companyId}-A"
+                //               select order
+                //               ).ToList();
+                // RQL version
+                var orders = session.Advanced.RawQuery<Order>(
+                    "from Orders " + // Occhio allo spazio!
+                    $"where Company=='companies/{companyId}-A'" + // Occhio al case! C maiuscolo
+                    "include Company"
+                );
+
+                var company = session.Load<Company>($"companies/{companyId}-A");
+
+                if (company == null)
+                {
+                    WriteLine($"Company #{companyId} not found.");
+                    return;
+                }
+
+                WriteLine($"Company #{companyId}");
+                WriteLine($"Orders for company {company.Name}");
+
+                foreach (var order in orders)
+                {
+                    WriteLine($"{order.Id} - {order.OrderedAt}");
+                }
+            }
+
         }
     }
 
